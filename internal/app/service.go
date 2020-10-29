cdeb5428c2bc29311a56d945a83c75f1a6e4ff2a
Konstantin Prudnikov <kpr@hellofresh.com>
2020-10-29 15:59:59 +0100

    feat: app get implementation

--- internal/app/service.go
+++ internal/app/service.go
@@ -1,4 +1,35 @@
 package app
 
-type Service struct {
+import (
+	"context"
+
+	"github.com/kpr-hellofresh/tinyurl/internal/domain/url"
+)
+
+// type Service struct {
+// 	URLAdder url.Adder
+// }
+
+// func (srv Service) ShortenURL() {
+
+// }
+
+// func (srv Service) GetURL() {
+
+// }
+
+type ShortenURL struct {
+	Adder url.Adder
+}
+
+func (srv ShortenURL) Handle() {
+
+}
+
+type GetURL struct {
+	Getter url.Getter
+}
+
+func (srv GetURL) Handle(ctx context.Context, id string) (url.URL, error) {
+	return srv.Getter.Get(ctx, id)
 }
